4ae44789e79f56aab8363e1c2c88cb9d9e0874b3
웹개발 <tkdduq241@naver.com>
2021-07-26 12:07:09 +0900

    7-26

--- src/main/java/com/sleep/spring/lesson03/bo/ReviewBO.java
+++ src/main/java/com/sleep/spring/lesson03/bo/ReviewBO.java
@@ -14,4 +14,12 @@ public class ReviewBO {
 	public Review getReview(int id) {
 		return reviewDAO.selectReview(id);
 	}
+	
+	public int addReview(int storeId, String menu, String userName, double point, String review) {
+		return reviewDAO.insertReview(storeId, menu, userName, point, review);
+	}
+	
+	public int addReview(Review review) {
+		return reviewDAO.insertReivewFromObject(review);
+	}
 }
--- src/main/java/com/sleep/spring/lesson03/dao/ReviewDAO.java
+++ src/main/java/com/sleep/spring/lesson03/dao/ReviewDAO.java
@@ -8,4 +8,15 @@ import com.sleep.spring.lesson03.model.Review;
 @Repository
 public interface ReviewDAO {
 	public Review selectReview(@Param("id") int id);
+	
+	public int insertReview(
+			@Param("storeId") int storeId, 
+			@Param("menu") String menu,
+			@Param("userName") String userName, 
+			@Param("point")  double point, 
+			@Param("review") String review
+			);
+
+	public int insertReivewFromObject(Review review);
+	
 }
