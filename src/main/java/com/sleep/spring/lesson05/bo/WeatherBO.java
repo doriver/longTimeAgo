cc18a0f2c93e3f2321ca1553516a34c4cad77299
doriver <tkdduq241@naver.com>
2021-08-06 04:10:17 +0900

    8-5집에서

--- src/main/java/com/sleep/spring/lesson05/bo/WeatherBO.java
+++ src/main/java/com/sleep/spring/lesson05/bo/WeatherBO.java
@@ -2,6 +2,7 @@ package com.sleep.spring.lesson05.bo;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,9 @@ public class WeatherBO {
 	
 	public List<Weather> getWeather() {
 		return weatherDAO.selectWeather();
+	}
+	
+	public int addWeather(String weather, double temperatures, double precipitation, String microDust, double windSpeed) {
+		return weatherDAO.insertWeather(weather, temperatures, precipitation, microDust, windSpeed);
 	}
 }
