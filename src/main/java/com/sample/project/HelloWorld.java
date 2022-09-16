c2ba61b16498531a933d430e83909beea92f8f7e
doriver <tkdduq241@naver.com>
2022-09-16 21:02:56 +0900

    springboot 웹 프로젝트 셈플

---
+++ src/main/java/com/sample/project/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.sample.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorld {
+
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String helloWorld() {
+		return "hello world";
+	}
+	
+    @RequestMapping("/jspp")
+    public String ex02() {  
+        return "test/hello"; 
+    }
+}
