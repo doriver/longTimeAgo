f39b0c985c64489b46a0bbe625177f1f7b4a79ef
doriver <tkdduq241@naver.com>
2022-10-18 21:52:28 +0900

    @Repository 제거

--- src/main/java/com/sb/basic/jpa/ex02/repository/CrudEntityRepository.java
+++ src/main/java/com/sb/basic/jpa/ex02/repository/CrudEntityRepository.java
@@ -5,11 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+
 
 import com.sb.basic.jpa.ex02.model.CrudEntity;
 
-@Repository
 public interface CrudEntityRepository extends JpaRepository<CrudEntity, String> {
 	
 	@Query(value = "select name, age from sample_member where name = :name", nativeQuery=true)
