2422a5a604a82941812548e6cca34fb307fda1dd
doriver <tkdduq241@naver.com>
2022-12-03 12:12:10 +0900

    mybatis이용

--- src/no/mybatis01/DBtest.java
+++ src/no/mybatis01/DBtest.java
@@ -1,4 +1,4 @@
-package no.mybatis;
+package no.mybatis01;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
--- src/no/mybatis01/MysqlService.java
+++ src/no/mybatis01/MysqlService.java
@@ -1,4 +1,4 @@
-package no.mybatis;
+package no.mybatis01;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
