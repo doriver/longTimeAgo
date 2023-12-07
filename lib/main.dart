35d62ad7951cc088df15ecf4455dd1fe006213df
DESKTOP-D0U2NNQ\LeeSang <tkdduq241@naver.com>
2023-12-07 14:47:28 +0900

    초기화

--- lib/main.dart
+++ lib/main.dart
@@ -12,33 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar( backgroundColor:Colors.blue ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('assets/camera.png', width: 150,),
-              Container(
-                width: 300,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('카메라 팝니다'),
-                    Text('금호동 3가'),
-                    Text('7000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4')
-                      ]
-                    )
-                  ]
-                )
-              )
-            ]
-          )
-        )
+        body: SizedBox()
       )
     );
   }
