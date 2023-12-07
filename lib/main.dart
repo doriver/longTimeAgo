74ab8751b3ecd28cbe1ce74fa5b44ba563ceb82f
DESKTOP-D0U2NNQ\LeeSang <tkdduq241@naver.com>
2023-12-07 15:30:07 +0900

    커스텀 위젯 설명

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,9 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+// 단축키 : stless + tab키  >>>  아래 커스텀위젯 기본 세팅 생김
+class MyWidget extends StatelessWidget { // 커스텀 위젯이 되려면 class안에 변수, 함수 많이 넣어야하고 그래서 extends StatelessWidget을 해주는거
+  const MyWidget({super.key}); // class 에 어떤 파라미터 넣을수 있는지 정의 부분
 
   @override
   Widget build(BuildContext context) {
