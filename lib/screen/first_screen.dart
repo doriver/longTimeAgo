3c85e2896e1e7761873dd4beac4a658ade1d1247
DESKTOP-D0U2NNQ\LeeSang <tkdduq241@naver.com>
2023-12-11 14:48:20 +0900

    화면등록 세팅 01

--- lib/screen/first_screen.dart
+++ lib/screen/first_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/widgets.dart';
 
 class FirstScreen extends StatefulWidget {
+  static String screenName = "FirstScreen";
 
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
 }
 
 class _FirstScreenState extends State<FirstScreen> {
-
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }
--- lib/screen/manager/screen_manager.dart
+++ lib/screen/manager/screen_manager.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_excercise_01/screen/first_screen.dart';
+
+Map<String, dynamic> kScreenMap = {
+  FirstScreen.screenName: const FirstScreen()
+};
 
 class ScreenManager {
   static void navitest() {
