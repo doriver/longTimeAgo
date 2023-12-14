9a3d5bc53b9607950700a6005e5597181ec81ded
DESKTOP-D0U2NNQ\LeeSang <tkdduq241@naver.com>
2023-12-14 16:47:33 +0900

    listview 아이템 사이 간격 추가

--- book_list/lib/screens/list_screen.dart
+++ book_list/lib/screens/list_screen.dart
@@ -12,11 +12,19 @@ class ListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('도서 목록'), backgroundColor: Colors.cyan),
-      body: ListView.builder( // 리스트뷰를 동적으로 구성, 대량의 데이터나 동적으로 변경되는 데이터 목록을 효율적으로 표시
+      body: ListView.separated( // 리스트뷰를 동적으로 구성, 대량의 데이터나 동적으로 변경되는 데이터 목록을 효율적으로 표시
         itemCount: books.length,
         itemBuilder: (context, index) {
           return BookTitle(book: books[index]);
-        }
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          // 각 아이템 사이에 구분선을 추가 (간격을 나타냅니다)
+          return Divider(
+            height: 10, // 원하는 간격 설정
+            color: Colors.transparent, // Divider의 색상을 투명으로 설정하여 간격만 나타냅니다.
+          );
+        },
+        padding: EdgeInsets.fromLTRB(0, 10, 0, 0)
       )
     );
   }
