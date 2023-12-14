e60c996bacfb7081b1b482ab19ca3b65fa5aeb19
DESKTOP-D0U2NNQ\LeeSang <tkdduq241@naver.com>
2023-12-14 13:47:33 +0900

    column 내용물 넓이 다를때

--- flutter_ui_excercise01/lib/columnRow.dart
+++ flutter_ui_excercise01/lib/columnRow.dart
@@ -7,11 +7,13 @@ class ColumnRow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        // mainAxisAlignment: MainAxisAlignment.end,
+        // crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Container(height: 50,width: 50, color:Colors.blue, margin: EdgeInsets.all(10)),
-          Container(height: 50,width: 50, color:Colors.blue, margin: EdgeInsets.all(10)),
-          Container(height: 50,width: 50, color:Colors.blue, margin: EdgeInsets.all(10)),
-          Container(height: 50,width: 50, color:Colors.blue, margin: EdgeInsets.all(10)),
+          Container(height: 50,width: 100, color:Colors.blue, margin: EdgeInsets.all(10)),
+          Container(height: 50,width: 150, color:Colors.blue, margin: EdgeInsets.all(10)),
+          Container(height: 50,width: 200, color:Colors.blue, margin: EdgeInsets.all(10)),
           Container(height: 50,width: 50, color:Colors.blue, margin: EdgeInsets.all(10)),
           Container(height: 50,width: 50, color:Colors.blue, margin: EdgeInsets.all(10)),
           Container(height: 50,width: 50, color:Colors.blue, margin: EdgeInsets.all(10))
